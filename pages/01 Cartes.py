10b9d68b4fdc1c3a5f1ec5767fa6f6baf1977379
Sidoine DAKO <audedako@gmail.com>
2023-03-09 14:29:05 +0100

    Update 01 Cartes.py

--- pages/01 Cartes.py
+++ pages/01 Cartes.py
@@ -3,8 +3,13 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import geopandas as gpd
+import folium
+import leafmap.foliumap as leafmap
+from folium.plugins import MarkerCluster
+from streamlit_folium import st_folium
 from scripts.dataSelector import DataSelector
 from scripts.dataCalculator import DataCalculator
+from scripts.modifyMap import markersMap
 
 # Set up the page
 st.set_page_config(page_title="Cartes")
@@ -63,10 +68,67 @@ dataCalc = DataCalculator(dataSel.df)
 globRatio = dataCalc.computeRatio()
 colGlobRat.metric("Ratio global",value=globRatio,help="Nombre de PDVs désservis par 1 dépôt en moyenne")
 
+## Répartition des dépôts et PDVs
+with st.expander("Répartition spatiale des dépôts et PDVs"):
+     st.markdown("""La carte ci-dessous est interactive. En cliquant sur un point,
+     vous avez la possibilité d'avoir les informations définies telles que le nom, le RCCM,
+     le nombre de bouteilles d'eau, de boissons gazeuses, de bières, etc.""")
+     dfDepot, dfPDV = dataSel.separateType()
+     m1 = folium.Map(location=[9.223351, 2.262477],zoom_start=6.5)
+     #clusterDepot = MarkerCluster(name="Dépôts").add_to(m1)
+     markersMap(dfDepot,m1,"#1A8EFA")
+     m1.fit_bounds(m1.get_bounds())
+     m1_folium = st_folium(m1)
+     st.write(dfDepot)
+
 ## Performances par département
 with st.expander("Performances par département"):
+    st.markdown("""La carte ci-dessous est interactive. En passant le curseur de la souris,
+    vous avez la possibilité de voir""")
     st.markdown("**Ratio 1 Dépôt\:PDVs**")
     ratioDep = dataCalc.computeRatio("Département").sort_values(by="Ratio",ascending=False)
     # Create shapefile with ratio
-    ratioDepShp = pd.merge(shpDep,ratioDep,how="left",left_on="adm1_name",right_on="Département")
-    st.write(ratioDepShp)
+    ratioDepShp = pd.merge(shpDep,ratioDep,how="right",left_on="adm1_name",right_on="Département")
+    # initialize the map and store it in a m object
+    m = folium.Map(location=[9.223351, 2.262477],zoom_start=6.5)
+    #folium.TileLayer('CartoDB positron',name="Light Map",control=False).add_to(m)
+
+    myscale = (ratioDepShp['Ratio'].quantile((0,0.25,0.5,0.75,1))).tolist()
+    m.choropleth(
+        geo_data=ratioDepShp,
+        name='Choropleth',
+        data=ratioDepShp,
+        columns=['Département','Ratio'],
+        key_on="feature.properties.Département",
+        fill_color='YlGnBu',
+        threshold_scale=myscale,
+        fill_opacity=.9,
+        line_opacity=0.2,
+        legend_name='Ratio Dépôt:PDVs',
+        smooth_factor=0
+    )
+
+    style_function = lambda x: {'fillColor': '#ffffff', 
+                            'color':'#000000', 
+                            'fillOpacity': 0.1, 
+                            'weight': 0.1}
+    highlight_function = lambda x: {'fillColor': '#000000', 
+                                    'color':'#000000', 
+                                    'fillOpacity': 0.50, 
+                                    'weight': 0.1}
+    NIL = folium.features.GeoJson(
+        ratioDepShp,
+        style_function=style_function, 
+        control=False,
+        highlight_function=highlight_function, 
+        tooltip=folium.features.GeoJsonTooltip(
+            fields=['Département','Ratio'],
+            aliases=['Département: ','Nbre PDVs par dépôts: '],
+            style=("background-color: white; color: #333333; font-family: arial; font-size: 12px; padding: 10px;") 
+        )
+    )
+    m.add_child(NIL)
+    #m.keep_in_front(NIL)
+    folium.LayerControl().add_to(m)
+    m.fit_bounds(m.get_bounds())
+    st_data = st_folium(m,height=500)
